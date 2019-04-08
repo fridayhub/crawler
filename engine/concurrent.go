5b3d330964390f2c9515c87c5ded2481dade5932
test <test@email.com>
2019-04-08 23:16:29 +0800

    pipline concurrent completed

--- engine/concurrent.go
+++ engine/concurrent.go
@@ -2,28 +2,33 @@ package engine
 
 import "log"
 
-type ConcurrentEngine struct{
-	Scheduler Scheduler
+type ConcurrentEngine struct {
+	Scheduler   Scheduler
 	WorkerCount int
 }
 
 type Scheduler interface {
 	Submit(Request)
-	ConfigureMasterWorkerChan(chan Request)
+	WorkerChan() chan Request
+	ReadyNotifier
+	Run()
+}
+
+type ReadyNotifier interface {
+	WorkerReady(chan Request)
 }
 
 func (c *ConcurrentEngine) Run(seeds ...Request) {
-	in := make(chan Request)
 	out := make(chan ParseResult)
 
-	c.Scheduler.ConfigureMasterWorkerChan(in)
+	c.Scheduler.Run()
 
 	for _, r := range seeds {
 		c.Scheduler.Submit(r)
 	}
 
 	for i := 0; i < c.WorkerCount; i++ { //According WorkerCount, concurrent all work
-		createWorker(in, out)
+		createWorker(c.Scheduler.WorkerChan(), out, c.Scheduler)
 	}
 
 	for {
@@ -38,10 +43,11 @@ func (c *ConcurrentEngine) Run(seeds ...Request) {
 	}
 }
 
-func createWorker(in chan Request, out chan ParseResult)  {
-
+func createWorker(in chan Request, out chan ParseResult, ready ReadyNotifier)  {
 	go func() {
 		for {
+			// tell scheduler i'm ready
+			ready.WorkerReady(in)
 			request := <- in
 			result, err := worker(request)
 			if err != nil {
